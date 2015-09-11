b3343a2e49ac249cf97cab865eb8b539e3ff6c0e
Gelio <voreny.gelio@gmail.com>
2015-09-11 13:30:13 +0200

    set up router

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
 (function() {
-    var app = angular.module('voteApp', []);
+    var app = angular.module('voteApp', ['ngRoute']);
+
+    app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'assets/templates/index.html',
+            controller: function() {
+
+            }
+        })
+            .otherwise('/');
+    }]);
 })();
